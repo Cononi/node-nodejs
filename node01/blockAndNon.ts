200d51f368f3edc9c1ace22edc40b6fda0a23d49
cononi <ekgoqhffo@gmail.com>
2023-03-17 00:35:08 +0900

    [add] const, let

--- node01/blockAndNon.ts
+++ node01/blockAndNon.ts
@@ -1,7 +1,7 @@
 
 
 // 블록킹 방식
-function longRunningTask(){
+function longRunningTask():void{
 	// 오래 걸리는 작업
 	console.log('작업 끝')
 }
@@ -10,11 +10,12 @@ longRunningTask()
 console.log('다음 작업')
 
 // 논블로킹 방식
-function longRunningTask(){
+console.log('---------------------------')
+function longRunningTaskNonBlock():void{
 	// 오래 걸리는 작업
 	console.log('작업 끝')
 }
 console.log('시작')
-longRunningTask()
-setTimeout(longRunningTask, 0)
+longRunningTaskNonBlock()
+setTimeout(longRunningTaskNonBlock, 0)
 console.log('다음 작업')
---
+++ node01/constAndlet.ts
@@ -0,0 +1,11 @@
+// 정상 출력되는 코드
+if(true) {
+	var x = 3;
+}
+console.log(x); //3
+
+// 에러가 발생하는 코드
+// if(true) {
+// 	const y = 3;
+// }
+// console.log(y);
